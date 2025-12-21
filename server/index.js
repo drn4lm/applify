70c3d9da7a7451d3698e8dbe0a14fdb410b11b77
Darren Lam <darren.lam2@ucalgary.ca>
2025-12-20 17:13:15 -0700

    Connected all dependencies

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MURL = process.env.MONGODB_URL;
+
+mongoose
+        .connect(MURL)
+        .then(() => {
+            console.log("DB connection successful.")
+            app.listen(PORT, () => {
+                console.log(`Server running on port: ${PORT}.`)
+            });
+        })
+        .catch((error) => console.log(error));
